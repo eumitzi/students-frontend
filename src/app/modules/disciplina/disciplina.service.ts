aa9b8b8bc798406541682578185a3e6a8e59bf7e
eumitzi <liliana.30>
2021-05-26 08:29:11 +0300

    fixed table header name

--- src/app/modules/disciplina/disciplina.service.ts
+++ src/app/modules/disciplina/disciplina.service.ts
@@ -1,6 +1,5 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
-import {NoteStudDisc} from '../../shared/components/model/noteStudDisc';
 import {Observable} from 'rxjs';
 import {NotePerDisc} from '../../shared/components/model/notePerDisc';
 import {NoteStdPerDisc} from '../../shared/components/model/noteStdPerDisc';
@@ -16,30 +15,16 @@ export class DisciplinaService {
     // this.noteStudDiscUrl = 'http://localhost:8085/api/student/note/idDisciplina/str';
     this.noteStudDiscUrl = 'http://localhost:8085/api/student/testp';
   }
+
   getRepos(numeStudent: string, prenumeStudent: string, numeDisciplina: string): Observable<NotePerDisc[]> {
-  let params = new HttpParams()
-    .set('numeStudent', numeStudent)
-    .set('prenumeStudent', prenumeStudent)
-    .set('numeDisciplina', numeDisciplina);
+    let params = new HttpParams()
+      .set('numeStudent', numeStudent)
+      .set('prenumeStudent', prenumeStudent)
+      .set('numeDisciplina', numeDisciplina);
 
-  console.log(params.toString());
+    console.log(params.toString());
 
-  return this.http.get<NoteStdPerDisc[]>(this.noteStudDiscUrl, {params});
-}
+    return this.http.get<NoteStdPerDisc[]>(this.noteStudDiscUrl, {params});
+  }
 
-  // public getNotePerDisc(note: NoteStdPerDisc): Observable<NoteStdPerDisc[]> {
-  //   return this.http.post<NoteStdPerDisc[]>(this.noteStudDiscUrl, note);
-  // }
-
-  // public getNotePerDisc(note: NoteStdPerDisc): Observable<> {
-  //   return this.http.get<NoteStdPerDisc[]>(this.noteStudDiscUrl, note);
-  // }
-//
-// sendPostRequest(data: any): Observable<any> {
-//   return this.httpClient.post<any>(YOUR-SERVER-URL, data);
-// }
-
-  // public getProducts(): Observable<Product[]> {
-  //   return this.http.get<Product[]>(`api/products/v1/`);
-  // }
 }
